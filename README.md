d7f49dd60ec731286eb39b97e149a72100d7943d
Javier Advani <javier.advani@gmail.com>
2020-04-12 17:38:34 +0200

    upload images, split script
    first commit uploading the dataset and train_test_split script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # car-crashes-detector
  Detect if a car is damaged using CNN 
+
+![preview](https://github.com/jadvani/car-crashes-detector/blob/master/preview.jpg)
+
+## Dataset
+
+The dataset used has been taken from this repository https://github.com/mghatee/Accident-Images-Analysis-Dataset
+<br>
+
+According to the original dataset:
+
+Folder 1 includes 2500 images with label "without-accident".
+Folder 2 includes 2398 images with label "with-accident".
+
+## How to run the code
+
+1. Generate the train/test/val split using train_test_split.py
+
---
+++ train_test_split.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 17:09:57 2020
+
+@author: Javier
+"""
+import os
+import numpy as np
+import shutil
+
+# # Creating Train / Val / Test folders (One time use)
+root_dir = 'F:/TFM_datasets/accidentes'
+posCls = '/1'
+negCls = '/2'
+
+elements = [posCls, negCls]
+
+os.makedirs(root_dir +'/train' + posCls)
+os.makedirs(root_dir +'/train' + negCls)
+os.makedirs(root_dir +'/val' + posCls)
+os.makedirs(root_dir +'/val' + negCls)
+os.makedirs(root_dir +'/test' + posCls)
+os.makedirs(root_dir +'/test' + negCls)
+
+# Creating partitions of the data after shuffeling
+for currentCls in elements:
+    src = root_dir+currentCls # Folder to copy images from
+    
+    allFileNames = os.listdir(src)
+    np.random.shuffle(allFileNames)
+    train_FileNames, val_FileNames, test_FileNames = np.split(np.array(allFileNames),
+                                                              [int(len(allFileNames)*0.7), int(len(allFileNames)*0.85)])
+    
+    
+    train_FileNames = [src+'/'+ name for name in train_FileNames.tolist()]
+    val_FileNames = [src+'/' + name for name in val_FileNames.tolist()]
+    test_FileNames = [src+'/' + name for name in test_FileNames.tolist()]
+    
+    print('Total images: ', len(allFileNames))
+    print('Training: ', len(train_FileNames))
+    print('Validation: ', len(val_FileNames))
+    print('Testing: ', len(test_FileNames))
+    
+    # Copy-pasting images
+    for name in train_FileNames:
+        shutil.copy(name, root_dir+"/train"+currentCls)
+    
+    for name in val_FileNames:
+        shutil.copy(name, root_dir+"/val"+currentCls)
+    
+    for name in test_FileNames:
+        shutil.copy(name, root_dir+"/test"+currentCls)
