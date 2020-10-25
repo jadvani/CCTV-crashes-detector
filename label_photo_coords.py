7f5f932d2a973b1a69f864905e422886ffb83627
Javier Advani <javier.advani@gmail.com>
2020-10-25 02:57:30 +0200

    script etiquetado más rapido

--- label_photo_coords.py
+++ label_photo_coords.py
@@ -9,12 +9,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets  import RectangleSelector
 import cv2
+import pyperclip
+
 xdata = np.linspace(0,9*np.pi, num=301)
 ydata = np.sin(xdata)
 
 fig, ax = plt.subplots()
 line, = ax.plot(xdata, ydata)
-img=cv2.imread("F:\\TFM_datasets\\extracted_frames\\000134\\14.jpg")
+img=cv2.imread("F:\\TFM_datasets\\extracted_frames\\000166\\101.jpg")
+
 
 
 def line_select_callback(eclick, erelease):
@@ -22,7 +25,9 @@ def line_select_callback(eclick, erelease):
     x2, y2 = erelease.xdata, erelease.ydata
 
     rect = plt.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2) )
-    print("["+str(int(x1))+","+str(int(y1))+","+str(int(x2-x1))+","+str(int(y2-y1))+"]")
+    coords = "["+str(int(x1))+","+str(int(y1))+","+str(int(x2-x1))+","+str(int(y2-y1))+"]"
+    print(coords)
+    pyperclip.copy(coords)
     ax.add_patch(rect)
 
 
@@ -32,4 +37,5 @@ rs = RectangleSelector(ax, line_select_callback,
                        interactive=True)
 
 plt.imshow(img[...,::-1])
-plt.pause(0.1)
+plt.pause(8)
+plt.close()
