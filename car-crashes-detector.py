5680c75f9914d059d1775eb5e2515be2904f533e
Javier Advani <javier.advani@gmail.com>
2020-11-18 00:00:48 +0100

    subida update de red con cambios para que de medidores crema

--- car-crashes-detector.py
+++ car-crashes-detector.py
@@ -18,7 +18,26 @@ from keras.applications.vgg16 import VGG16, decode_predictions
 
 modeloVGG16 = VGG16()
 #%%
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+#%%
 # dimensions of our images.
 img_width, img_height = 28, 28
 
